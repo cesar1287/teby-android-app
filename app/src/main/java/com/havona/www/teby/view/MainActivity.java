cf002c16d239f459a5c54efe8c424327fe40f358
César Rodrigues <rasec1287@hotmail.com>
2016-11-01 14:46:30 -0200

    Ajustado MVC no projeto

--- app/src/main/java/com/havona/www/teby/view/MainActivity.java
+++ app/src/main/java/com/havona/www/teby/view/MainActivity.java
@@ -2,6 +2,7 @@ package com.havona.www.teby.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -79,7 +80,7 @@ public class MainActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
