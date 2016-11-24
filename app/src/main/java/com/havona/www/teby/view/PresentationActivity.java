779a4161dafd80da3657779f1f90917b2f1a77e6
César Rodrigues <rasec1287@hotmail.com>
2016-11-24 17:23:45 -0300

    Adicionado arquivo java para a Activity de apresentação

--- app/src/main/java/com/havona/www/teby/view/PresentationActivity.java
+++ app/src/main/java/com/havona/www/teby/view/PresentationActivity.java
@@ -3,6 +3,8 @@ package com.havona.www.teby.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.havona.www.teby.R;
+
 public class PresentationActivity extends AppCompatActivity {
 
     @Override
